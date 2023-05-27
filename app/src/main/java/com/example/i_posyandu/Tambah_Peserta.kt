db2583640750126f600a070b97f0041ffb33cbbe
Rafikmato <rafikmato@gmail.com>
2023-05-27 18:34:03 +0800

    Menyelesaikan conflict devOkta

---
+++ app/src/main/java/com/example/i_posyandu/Tambah_Peserta.kt
@@ -0,0 +1,11 @@
+package com.example.i_posyandu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tambah_Peserta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_peserta)
+    }
+}
---
+++ app/src/main/java/com/example/i_posyandu/kader_brcode.kt
@@ -0,0 +1,11 @@
+package com.example.i_posyandu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kader_brcode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kader_brcode)
+    }
+}
