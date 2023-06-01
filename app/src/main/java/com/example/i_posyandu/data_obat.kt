566bde3c6a6ca4acf63938a9e08aed98cee4a04c
Rafikmato <rafikmato@gmail.com>
2023-06-01 15:30:35 +0800

    terbaru

--- app/src/main/java/com/example/i_posyandu/data_obat.kt
+++ app/src/main/java/com/example/i_posyandu/data_obat.kt
@@ -3,11 +3,9 @@ package com.example.i_posyandu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class data_obat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.untuk_peserta)
-
+        setContentView(R.layout.activity_data_obat)
     }
 }
